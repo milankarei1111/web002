eb0d280ab98bb10cef1bdbd0d104290967597f1b
wanru_tsai <milankarei1111@gmail.com>
2019-11-05 17:05:37 +0800

    modfiy 表格框架 category/index.blade.php add 表單框架 category/create.blade.php

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,24 +8,78 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-sm-12">
             <div class="box box-primary">
                 <div class="box-header">
                     <h3 class="box-title">分類表</h3>
                 </div>
                 {{-- /.box-header --}}
-                 <!-- form start -->
-                 <form action="">
-                    <div class="box-body">
-                        
+                <div class="box-body">
+                    <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                    <div class="dataTables_length" id="example1_length">
+                                        <label>顯示<select name="example1_length" aria-controls="example1" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select>
+                                        </label>
+                                    </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div id="example1_filter" class="dataTables_filter">
+                                    <label>搜尋:
+                                        <input type="search" class="form-control input-sm" placeholder="" aria-controls="example1">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                                    <thead>
+                                        <tr role="row">
+                                            <th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 130px;">分類序號</th>
+                                            <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 162px;">分類名稱</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr role="row" class="odd">
+                                            <td class="sorting_1">顯示顏色列</td>
+                                            <td>未顯示顏色類</td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>分類序號</th>
+                                            <th>分類名稱</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">顯示 1 to 57 of 57 entries</div>
+                            </div>
+                            <div class="col-sm-7">
+                                    <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                                        <ul class="pagination">
+                                            <li class="paginate_button previous disabled" id="example1_previous">
+                                                <a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a>
+                                            </li><li class="paginate_button active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a>
+                                            </li><li class="paginate_button next disabled" id="example1_next">
+                                                <a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">Next</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                            </div>
+                        </div>
                     </div>
-                    {{-- /.box-body --}}
+                </div>
+                {{-- /.box-body --}}
 
-                    <div class="box-footer">
+                <div class="box-footer">
 
-                    </div>
-                    {{-- /.box-footer --}}
-                </form>
+                </div>
+                {{-- /.box-footer --}}
             </div>
         </div>
     </div>
